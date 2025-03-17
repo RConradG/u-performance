672530fa3edb0f2b9b3ac4f87aeacd2ff99e8109
RConradGe <rcgadduang@gmail.com>
2025-03-17 11:36:04 -0700

    wip: added workoutSchema, updated user to include workouts

--- models/user.js
+++ models/user.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
+const workoutSchema= mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  muscleGroup: {
+    type: String
+  },
+  exercise: {
+    type: String
+  },
+  weight: {
+    type: Number
+  },
+  sets: {
+    type: Number
+  },
+  repGoal: {
+    type: Number
+  },
+  actualReps: {
+    type: Number
+  },
+  intensity: {
+    type: Number
+  },
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +37,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  workout: [workoutSchema],
 });
 
 const User = mongoose.model('User', userSchema);
